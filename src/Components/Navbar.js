a7383d97042500bb8861644ca25f0715547aaa42
FrostXk9 <brandonroulstone465@gmail.com>
2023-08-27 22:54:26 +0200

    .

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -19,7 +19,7 @@ const NavbarLayout = () => {
     <div className="Nav-bar">
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand>React-Bootstrap</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
