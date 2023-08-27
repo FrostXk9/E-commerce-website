adf029cfa87dfcb028c8237aca06ecaf8155a5b8
FrostXk9 <brandonroulstone465@gmail.com>
2023-08-27 22:52:50 +0200

    .

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShop } from '@fortawesome/free-solid-svg-icons';
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
 
 const NavbarLayout = () => {
 
@@ -21,8 +23,8 @@ const NavbarLayout = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link to="/" as={Link} className="icon-nav">Products</Nav.Link>
-                <Nav.Link to="/address" as={Link} className="icon-nav">Register</Nav.Link>
+                <Nav.Link to="/" as={Link} className="icon-nav"><FontAwesomeIcon icon={faShop} />Shop</Nav.Link>
+                <Nav.Link to="/address" as={Link} className="icon-nav"><FontAwesomeIcon icon={faAddressCard} /> Register</Nav.Link>
                 <Nav.Link to= "/cart" as={Link} className="icon-nav"><FontAwesomeIcon icon={faBagShopping} />{CartProducts.length}</Nav.Link>
               </Nav>
             </Navbar.Collapse>
